a4a24c15d299939ce06e886c5a0e2c4962bc367b
Ayaz Alavi <ayaz.alavi@gmail.com>
2021-11-21 15:38:59 +0500

    Main application router

--- src/Navigators/Application.tsx
+++ src/Navigators/Application.tsx
@@ -4,25 +4,27 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import { StartupContainer } from '@/Containers'
 import { useTheme } from '@/Hooks'
-import MainNavigator from './Main'
+import AuthenticationNavigator from './Authentication'
 import { navigationRef } from './utils'
 
 const Stack = createStackNavigator()
 
 // @refresh reset
 const ApplicationNavigator = () => {
-  const { Layout, darkMode, NavigationTheme } = useTheme()
+  const { Layout, darkMode, Colors, NavigationTheme } = useTheme()
   const { colors } = NavigationTheme
-
   return (
-    <SafeAreaView style={[Layout.fill, { backgroundColor: colors.card }]}>
+    <SafeAreaView style={[Layout.fill, { backgroundColor: colors.primary }]}>
       <NavigationContainer theme={NavigationTheme} ref={navigationRef}>
-        <StatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} />
+        <StatusBar
+          barStyle={darkMode ? 'light-content' : 'dark-content'}
+          hidden={true}
+        />
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Startup" component={StartupContainer} />
           <Stack.Screen
-            name="Main"
-            component={MainNavigator}
+            name="Authentication"
+            component={AuthenticationNavigator}
             options={{
               animationEnabled: false,
             }}
