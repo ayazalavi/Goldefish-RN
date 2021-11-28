88a59c91bf060bb055c6324e01a07fd8b7bda4a0
Ayaz Alavi <ayaz.alavi@gmail.com>
2021-11-29 03:06:09 +0500

    base request and data models

---
+++ src/Services/modules/base/profile.ts
@@ -0,0 +1,17 @@
+export type Profile = {
+  name: any
+  location: any
+  dateOfBirth: any
+  isEmailVerified: boolean
+  tags: Array<string>
+  friends: Array<string>
+  subscribtions: Array<string>
+  reviews: Array<string>
+  _id: string
+  phone: string
+  email: string
+  password: string
+  username: string
+  createdAt: string
+  updatedAt: string
+}
---
+++ src/Services/modules/base/response.ts
@@ -0,0 +1,6 @@
+export class Response {
+  public data: any
+  public message: string = ''
+  public status: number = 200
+  public success: boolean = true
+}
